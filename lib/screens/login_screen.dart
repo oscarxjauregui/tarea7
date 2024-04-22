1afbd8bc3e364942b29e72b086f8d72f3ec3af3f
oscarxjauregui <19031369@itcelaya.edu.mx>
2024-04-22 15:18:46 -0600

    ya esta conectado al firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:tarea7/services/email_auth_firebase.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,11 +11,57 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final authFirebase = EmailAuthFirebase();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final txtEmail = TextFormField(
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(border: OutlineInputBorder()),
+    );
+    final txtPassword = TextFormField(
+      controller: _passwordController,
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      decoration: const InputDecoration(border: OutlineInputBorder()),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login')
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                height: 500,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    txtEmail,
+                    const SizedBox(height: 10),
+                    txtPassword,
+                    const SizedBox(height: 10),
+                    SignInButton(
+                      Buttons.Email,
+                      onPressed: () {},
+                    ),
+                    const SizedBox(height: 10),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Crear cuenta'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/services/email_auth_firebase.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class EmailAuthFirebase {
+  final auth = FirebaseAuth.instance;
+  final usersRef = FirebaseFirestore.instance.collection('users');
+
+  Future<bool> signUpUser({
+    required String name,
+    required String password,
+    required String email,
+    //required String rol,
+    //File? imageFile,
+  }) async {
+    try {
+      final userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user != null) {
+        userCredential.user!.sendEmailVerification();
+        await usersRef.doc(userCredential.user!.uid).set({
+          'nombre': name,
+          'email': email,
+          //'rol': rol,
+        });
+        //if (imageFile != null) {
+        //  await uploadImage(userCredential.user!.uid, imageFile);
+        //}
+        return true;
+      }
+      return false;
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
+  }
+
+  /*Future<void> uploadImage(String userId, File imageFile) async {
+    try {
+      final storageRef =
+          FirebaseStorage.instance.ref().child('avatar').child('$userId.jpg');
+      await storageRef.putFile(imageFile);
+      final imageUrl = await storageRef.getDownloadURL();
+
+      await FirebaseFirestore.instance.collection('avatar').doc(userId).set({
+        'avatarUrl': imageUrl,
+      });
+    } catch (e) {
+      print("Error al subir la imagen a firestore sotrage: $e");
+    }
+  }
+
+  Future<bool> signInUser(
+      {required String password, required String email}) async {
+    var band = false;
+    final userCredential =
+        await auth.signInWithEmailAndPassword(email: email, password: password);
+    if (userCredential.user != null) {
+      if (userCredential.user!.emailVerified) {
+        band = true;
+      }
+    }
+    return band;
+  }*/
+}
